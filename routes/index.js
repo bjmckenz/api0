e62ada84403e8e8b2760de4130b3daa5d12c0c1e
bruce mckenzie <bjmckenz@gmail.com>
2024-03-11 17:50:25 -0700

    Added /container/(id) route and POST /container for container creation

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,19 @@ db.pragma('journal_mode = WAL');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('<body>You probably want to hit the <a href="http://127.0.0.0:5173/">Front End</a> because this is just the API</body>');
 });
 
 router.get('/containers0', function(req, res, next) {
   res.json({containers: [
-    {id:1, number:'123123', size:20,
+    {containerNumber:1, number:'123123', size:20,
       shipdate:'2024-03-01', shipname:'Maersk Star of India', portname: 'Long Beach',
         customerid: 22, customerName:'acme', shipid:99},
-    {id:1, number:'123123776', size:20,
+    {containerNumber:1, number:'123123776', size:20,
         shipdate:'2024-02-01', shipname:'Cosco Take over World', portname: 'San Pedro',
           customerid: 22, customerName:'acme',
           shipid:234},
-    {id:1, number:'23423', size:10,
+    {containerNumber:1, number:'23423', size:10,
           shipdate:'2024-03-02', shipname:'Evergreen Fair Seas', portname: 'San Pedro',
             customerid: 15, customerName:'Springfield',
             shipid:123},
@@ -27,10 +27,32 @@ router.get('/containers0', function(req, res, next) {
 });
 
 router.get('/containers', function(req, res, next) {
-  const stmt = db.prepare('SELECT id, containerSize, dateContainerShipped, nameOfShip from containers');
+  const stmt = db.prepare('SELECT containerNumber, containerSize, dateContainerShipped, nameOfShip from containers');
   containers = stmt.all();
-  console.log(containers);
+  //console.log(containers);
   res.json({containers: containers});
+});
+
+router.get('/container/:containerNumber', function(req, res, next) {
+  const stmt = db.prepare('SELECT containerNumber, containerSize, dateContainerShipped, nameOfShip from containers WHERE containerNumber = ?');
+  containers = stmt.all(req.params.containerNumber);
+  //console.log(containers);
+  res.json({containers: containers});
+});
+
+router.post('/container', function(req, res, next) {
+  const newContainer = req.body;
+  console.log(newContainer);
+
+
+  const stmt = db.prepare('INSERT INTO containers (containerNumber, containerSize, dateContainerShipped, nameOfShip) VALUES (?, ?, ?, ?)');
+  const info = stmt.run(newContainer.containerNumber, newContainer.containerSize, newContainer.dateContainerShipped, newContainer.nameOfShip);
+
+  if (info.changes !== 1) {
+    res.status(400).json({error: 'Container not added'});
+    return
+  }
+  res.json({containerNumber: newContainer.containerNumber});
 });
 
 module.exports = router;
